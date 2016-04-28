dcec6d63d4cff33f8d51a654cc4d46b899b7ab60
Travisinseattle <travisinseattle@gmail.com>
2016-04-28 10:40:26 -0700

    Attempted to add barcode to scanActivity.

--- app/src/main/java/com/example/totes_rewards/totesrewards/ScanActivity.java
+++ app/src/main/java/com/example/totes_rewards/totesrewards/ScanActivity.java
@@ -1,13 +1,23 @@
 package com.example.totes_rewards.totesrewards;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.annotation.RequiresPermission;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
 
-import com.google.android.gms.vision.Frame;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
 
@@ -15,37 +25,174 @@ import java.io.IOException;
 
 public class ScanActivity extends AppCompatActivity {
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_scan);
-        Bitmap myQRCode = null;
-        try {
-            myQRCode = BitmapFactory.decodeStream(
-                    getAssets().open("qr_code.jpg")
-            );
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
-        BarcodeDetector barcodeDetector =
-                new BarcodeDetector.Builder(this)
-                        .setBarcodeFormats(Barcode.QR_CODE)
-                        .build();
+        final SurfaceView cameraView = (SurfaceView) findViewById(R.id.camera_view);
+        final TextView barcodeInfo = (TextView) findViewById(R.id.code_info);
 
-        Frame myFrame = new Frame.Builder()
-                .setBitmap(myQRCode)
+        final BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.QR_CODE)
                 .build();
 
-        SparseArray<Barcode> barcodes = barcodeDetector.detect(myFrame);
+        final CameraSource cameraSource = new CameraSource.Builder(this, barcodeDetector)
+                .setRequestedPreviewSize(640, 480)
+                .build();
 
-        // Check if at least one barcode was detected
-        if(barcodes.size() != 0) {
 
-            // Print the QR code's message
-            Log.d("My QR Code's Data",
-                    barcodes.valueAt(0).displayValue
-            );
-        }
+        cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                try {
+                    cameraSource.start(cameraView.getHolder());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+
+                if (barcodes.size() != 0) {
+                    barcodeInfo.post(new Runnable() {    // Use the post method of the TextView
+                        public void run() {
+                            barcodeInfo.setText(    // Update the TextView
+                                    barcodes.valueAt(0).displayValue
+                            );
+                        }
+                    });
+                }
+            }
+        });
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Scan Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.totes_rewards.totesrewards/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Scan Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.totes_rewards.totesrewards/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+//        TextView test = (TextView) findViewById(R.id.barcodeResult);
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_scan);
+//        Bitmap myQRCode = null;
+//        try {
+//            myQRCode = BitmapFactory.decodeStream(
+//                    getAssets().open("qr_code.jpg")
+//            );
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        BarcodeDetector barcodeDetector =
+//                new BarcodeDetector.Builder(this)
+//                        .setBarcodeFormats(Barcode.QR_CODE)
+//                        .build();
+//
+//        Frame myFrame = new Frame.Builder()
+//                .setBitmap(myQRCode)
+//                .build();
+//
+//        SparseArray<Barcode> barcodes = barcodeDetector.detect(myFrame);
+//
+//        StringBuilder sb = new StringBuilder();
+//        // Check if at least one barcode was detected
+//        if(barcodes.size() != 0) {
+//
+//
+//            for (int i = 0; i < barcodes.size(); i++) {
+//                sb.append(barcodes.valueAt(0).rawValue.toString());
+//            }
+//
+//            //System.out.print(sb.toString());
+//
+//            // Print the QR code's message
+//            Log.d("My QR Code's Data",
+//                    //barcodes.valueAt(0).displayValue
+//                    sb.toString()
+//            );
+//        }
+//
+//        if(test != null) {
+//            test.setText("mkpmj;iponj");
+//        } else {
+//            Log.d("This Sucks.",
+//                    //barcodes.valueAt(0).displayValue
+//                    sb.toString()
+//            );
+//        }
+//    }
 }
