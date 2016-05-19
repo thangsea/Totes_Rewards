c29f970a60fc4de46d9874c4fdd47aa19a7faff6
Travisinseattle <travisinseattle@gmail.com>
2016-05-19 10:02:03 -0700

    Adjust sleep timer on main menu

--- app/src/main/java/com/example/totes_rewards/totesrewards/MainActivity.java
+++ app/src/main/java/com/example/totes_rewards/totesrewards/MainActivity.java
@@ -4,27 +4,27 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
     ProgressBar spinner;
-    final Intent menuIntent = new Intent(this, MenuActivity.class);
+    View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
         spinner = (ProgressBar) findViewById(R.id.progressBar);
-
+        open(findViewById(R.id.progressBar));
+        StartMenu();
     }
 
     public void open(View view) {
         spinner.setIndeterminate(true);
-        final int totalProgressTime = 10;
+        final int totalProgressTime = 50;
 
         final Thread t = new Thread() {
 
@@ -34,8 +34,9 @@ public class MainActivity extends AppCompatActivity {
                 int jumpTime = 0;
                 while (jumpTime < totalProgressTime) {
                     try {
-                        sleep(10);
+                        sleep(1000);
                         jumpTime += 5;
+                        Log.d("Message", "It's Working!");
                     } catch (InterruptedException e) {
                         // TODO Auto-generated catch block
                         e.printStackTrace();
@@ -45,6 +46,14 @@ public class MainActivity extends AppCompatActivity {
 
             }
         };
+        t.start();
+    }
+
+
+
+    public void StartMenu() {
+        final Intent menuIntent = new Intent(this, MenuActivity.class);
         startActivity(menuIntent);
     }
+
 }
